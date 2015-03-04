e7a9967f302b67425ecfb98dce9233ceb5adb978
Eugen Soloviov <suenot@gmail.com>
2015-03-04 19:22:14 +0300

    watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 var gulp = require('gulp'),
-    $ = require('gulp-load-plugins')();
+	$ = require('gulp-load-plugins')();
+
+gulp.task('watch', function () {
+	gulp.watch(['./sass/*.sass'], ['default']);
+});
 
 gulp.task('default', function () {
-    return gulp.src(['sass/screen.sass'])
-        .pipe($.sourcemaps.init())
-        .pipe($.sass({
-        	indentedSyntax: true,
-            errLogToConsole: true
-        }))
-        .pipe($.autoprefixer('last 3 version'))
-        .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest('css'));
+	return gulp.src(['sass/*.sass'])
+		.pipe($.sourcemaps.init())
+		.pipe($.sass({
+			indentedSyntax: true,
+			errLogToConsole: true
+		}))
+		.pipe($.autoprefixer('last 3 version'))
+		.pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest('css'));
 });
